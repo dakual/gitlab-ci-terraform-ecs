3d229a156a4e1dcdb36602ef87cf457a6b371166
Daghan Kurtay Altunsoy <daghan.altunsoy@gmail.com>
2023-02-12 17:41:11 +0300

    Initial commit

---
+++ infra/outputs.tf
@@ -0,0 +1,15 @@
+output "rds_host" {
+  value = module.rds.rds_host
+}
+
+output "rds_dbname" {
+  value = module.rds.rds_dbname
+}
+
+output "rds_username" {
+  value = module.rds.rds_username
+}
+
+output "rds_password" {
+  value = module.rds.rds_password
+}
