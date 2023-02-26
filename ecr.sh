647062500b90c0a638ffbbf8d7f5ddfd09a2dd9c
Daghan Kurtay Altunsoy <daghan.altunsoy@gmail.com>
2023-02-26 18:02:29 +0300

    Initial commit

--- ecr.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -
-REPO_NAME="nginx1"
-REPO_ARN=$(aws ecr describe-repositories --repository-name ${REPO_NAME} --query "repositories[].repositoryArn" --output text 2>&1)
-if [[ "$REPO_ARN" == *"RepositoryNotFoundException"* ]]; then
-  REPO_ARN=$(aws ecr create-repository --repository-name ${REPO_NAME} --query "repository.repositoryArn" --output text 2>&1)  
-fi
-
-echo ${REPO_ARN}
-
